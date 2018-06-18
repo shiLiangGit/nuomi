b0e5730ec96ef5415023f39dcc8f2dd2c9373cb0
shiLiangGit <2628666865@qq.com>
2018-06-18 23:42:15 +0800

    初始化tp5.0糯米项目

---
+++ application/admin/controller/Category.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sj
+ * Date: 2018/6/18
+ * Time: 17:18
+ */
+
+namespace app\admin\controller;
+use think\Controller;
+
+class Category extends Controller
+{
+    public function cateList(){
+        return view();
+    }
+}
---
+++ application/admin/controller/Index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sj
+ * Date: 2018/6/18
+ * Time: 16:30
+ */
+namespace app\admin\controller;
+use think\Controller;
+
+class Index extends Controller
+{
+    public function index(){
+
+        return view();
+    }
+    public function welcome(){
+
+        return view();
+    }
+}
